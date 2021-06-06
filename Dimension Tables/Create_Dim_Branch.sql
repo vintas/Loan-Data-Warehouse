9ae3dc8ef2b343c7c7f2c03e9a95ebd10ed61c1f
roshi-git <abhees_private@outlook.com>
2021-06-06 18:03:15 +0530

    Added Dim_Branch and Dim_Loans

---
+++ Dimension Tables/Create_Dim_Branch.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[Dim_Branch] (
+	[Branch ID] NVARCHAR(20) NOT NULL PRIMARY KEY,
+	[Branch Name] NVARCHAR(20),
+	[Branch Location] NVARCHAR(20),
+	[Number of Employees] INT,
+	[Number of Customers] INT,
+	[Branch Type] NVARCHAR(10),
+	[City] NVARCHAR(20),
+	[STATE] NVARCHAR(20),
+	[COUNTRY] NVARCHAR(20)
+)

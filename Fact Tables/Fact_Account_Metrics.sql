2aa62a25e37fcd2f7a7c0f31a3e58496dd021df2
Avinash <vintas@users.noreply.github.com>
2021-06-08 15:32:15 +0530

    Correct Account Metrics Fact Table

--- Fact Tables/Fact_Account_Metrics.sql
+++ Fact Tables/Fact_Account_Metrics.sql
@@ -1,8 +1,9 @@
 CREATE TABLE [dbo].[Fact_Account_Metrics](
-	[Account Key] [bigint] IDENTITY(1,1) NOT NULL,
+	[Fact_Account Key] [bigint] IDENTITY(1,1) NOT NULL,
+	[Account Key] [int] NOT NULL,
 	[Date Key] [int] NOT NULL,
     [Current Account Balance] [decimal](18, 2) NULL,
-	[Has a Loan] [tinyint] NOT NULL,
+	[Has a Loan] [nvarchar](50) NOT NULL,
     [Number of Loans] [int] NOT NULL,
 	[Lineage Key] [int] NULL
 )

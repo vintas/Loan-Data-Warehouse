de6cc375dbc92c4fc6a70d277605351bba675503
roshi-git <abhees_private@outlook.com>
2021-06-08 11:46:32 +0530

    Minor change to Dim_Account

--- Dimension Tables/Create_Dim_Account.sql
+++ Dimension Tables/Create_Dim_Account.sql
@@ -2,6 +2,6 @@ CREATE TABLE [dbo].[Dim_Account](
 	[Account Key] INT NOT NULL PRIMARY KEY,
 	[Source Key] NVARCHAR(10) NOT NULL,
 	[Account Open Date] INT NOT NULL,
-	[Account Type] INT NOT NULL,
+	[Account Type] NVARCHAR(10) NOT NULL,
 	[Lineage Key] INT NOT NULL
 )
